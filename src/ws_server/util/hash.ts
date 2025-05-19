ef8a220fa8f5f9ae989c97a1436096d081399ac7
Igor Bartashevich <i.bartashevich@ftsdev.com>
2025-05-19 21:44:10 +0200

    feat: types for hash

--- src/ws_server/util/hash.ts
+++ src/ws_server/util/hash.ts
@@ -1,5 +1,5 @@
 import { createHash } from "node:crypto"
 
-export const hashPassword = (data: string) => {
+export const hashPassword: (data:string) => string = (data: string) => {
     return createHash('sha256').update(data).digest('hex')
 }
